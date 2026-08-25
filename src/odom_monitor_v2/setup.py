85f1958f67a414569c3b5a2c37f4ec9d8e3c55d0
Deepak R <wssedd18@gmail.com>
2026-08-25 21:49:32 +0530

    Version 2 Initialisation

---
+++ src/odom_monitor_v2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'odom_monitor_v2'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='deepak-r',
+    maintainer_email='wssedd18@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            "odom_drift_updater = odom_monitor_v2.monitor_node:main",
+        ],
+    },
+)
